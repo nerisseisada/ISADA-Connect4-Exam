3deafbdb51e482f1685d8983ee3821b6f5449fe3
Nerisse <61720945+nerisseisada@users.noreply.github.com>
2023-02-15 07:40:48 +0800

    Create main.php

---
+++ main.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ISADA Connect 4 Technical Exam</title>
+    <link rel="stylesheet" href="main.css">
+</head>
+
+<body>
+
+    <h1>CONNECT 4 BOARD GAME</h1>
+    <h2 id="turn">Red's Turn</h2>
+
+    <div class="grid">
+        <div class="column" id="c0">
+            <div class="cell" id ="c0-5"></div>
+            <div class="cell" id ="c0-4"></div>
+            <div class="cell" id ="c0-3"></div>
+            <div class="cell" id ="c0-2"></div>
+            <div class="cell" id ="c0-1"></div>
+            <div class="cell" id ="c0-0"></div>
+        </div>
+		
+        <div class="column" id="c1">
+            <div class="cell" id ="c1-5"></div>
+            <div class="cell" id ="c1-4"></div>
+            <div class="cell" id ="c1-3"></div>
+            <div class="cell" id ="c1-2"></div>
+            <div class="cell" id ="c1-1"></div>
+            <div class="cell" id ="c1-0"></div>
+        </div>
+
+        <div class="column" id="c2">
+            <div class="cell" id ="c2-5"></div>
+            <div class="cell" id ="c2-4"></div>
+            <div class="cell" id ="c2-3"></div>
+            <div class="cell" id ="c2-2"></div>
+            <div class="cell" id ="c2-1"></div>
+            <div class="cell" id ="c2-0"></div>
+        </div>   
+        
+        <div class="column" id="c3">
+            <div class="cell" id ="c3-5"></div>
+            <div class="cell" id ="c3-4"></div>
+            <div class="cell" id ="c3-3"></div>
+            <div class="cell" id ="c3-2"></div>
+            <div class="cell" id ="c3-1"></div>
+            <div class="cell" id ="c3-0"></div>
+        </div>
+        
+        <div class="column" id="c4">
+            <div class="cell" id ="c4-5"></div>
+            <div class="cell" id ="c4-4"></div>
+            <div class="cell" id ="c4-3"></div>
+            <div class="cell" id ="c4-2"></div>
+            <div class="cell" id ="c4-1"></div>
+            <div class="cell" id ="c4-0"></div>
+        </div>
+        
+        <div class="column" id="c5">
+            <div class="cell" id ="c5-5"></div>
+            <div class="cell" id ="c5-4"></div>
+            <div class="cell" id ="c5-3"></div>
+            <div class="cell" id ="c5-2"></div>
+            <div class="cell" id ="c5-1"></div>
+            <div class="cell" id ="c5-0"></div>
+        </div>
+
+        <div class="column" id="c6">
+            <div class="cell" id ="c6-5"></div>
+            <div class="cell" id ="c6-4"></div>
+            <div class="cell" id ="c6-3"></div>
+            <div class="cell" id ="c6-2"></div>
+            <div class="cell" id ="c6-1"></div>
+            <div class="cell" id ="c6-0"></div>
+        </div>
+    </div>
+
+</body>
+
+<script src="./app.js"></script>
+
+</html>
